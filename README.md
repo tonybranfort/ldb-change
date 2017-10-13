40707dac791fdaa4b43448e1471966a5d616f21a
Tony Branfort <tonybranfort@yahoo.com>
2017-10-12 22:34:59 -0500

    v1.0.0

---
+++ README.md
@@ -0,0 +1,82 @@
+# ldb-change
+Basic comparison of object literals and arrays to get differences by path
+
+## Functions
+
+<dl>
+<dt><a href="#getDiffs">getDiffs(lObj, rObj, path, options)</a> ⇒ <code><a href="#Diff">Array.&lt;Diff&gt;</a></code></dt>
+<dd><p>Return an array of paths from an object (<code>lObj</code>) that don&#39;t match another object (<code>rObj</code>) and the values of each.<br>  Equality comparisons include String, Number, Boolean, Date or set custom comparison with options.compare by path
+  Each element or key in an Array or literal/plain Object is evaluated (unless options.stopAt set for path)</p>
+</dd>
+</dl>
+
+## Typedefs
+
+<dl>
+<dt><a href="#Diff">Diff</a> : <code>Object</code></dt>
+<dd></dd>
+<dt><a href="#GetDiffsOptions">GetDiffsOptions</a> : <code>Object</code></dt>
+<dd></dd>
+</dl>
+
+<a name="getDiffs"></a>
+
+## getDiffs(lObj, rObj, path, options) ⇒ [<code>Array.&lt;Diff&gt;</code>](#Diff)
+Return an array of paths from an object (`lObj`) that don't match another object (`rObj`) and the values of each.  
+  Equality comparisons include String, Number, Boolean, Date or set custom comparison with options.compare by path
+  Each element or key in an Array or literal/plain Object is evaluated (unless options.stopAt set for path)
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| lObj | <code>Any</code> |  |
+| rObj | <code>Any</code> |  |
+| path | <code>String</code> \| <code>undefined</code> | Path to object attribute in dot notation |
+| options | [<code>GetDiffsOptions</code>](#GetDiffsOptions) \| <code>undefined</code> |  |
+
+**Example**  
+```js
+// returns [{path: 'a', lObj: 3, rObj: undefined}]
+ getDiffs({a: 5}, {c: 22})
+```
+**Example**  
+```js
+// returns [{path: 'a.b', lObj: 3, rObj: 5}, ]
+  getDiffs({a: {b: 3}}, {a: {b: 5}})
+```
+**Example**  
+```js
+// returns [{path: 'a[0].b.c', lObj: 5, rObj: 7}, ]
+  getDiffs({a: [{b: {c: 5}}]}, {a: [{b: {c: 7}}]})
+```
+**Example**  
+```js
+// returns [{path: 'a.b', lObj: {c: 8}, rObj: {c: 7}}, ]
+  getDiffs({a: {b: {c: 8}}}, {a: {b: {c: 7}}}, undefined, {stopAt: {"a.b": true}})
+```
+**Example**  
+```js
+// returns []  (no Diffs because of compare function at a path )
+  getDiffs({a: 1}, {a: '1'}, undefined, {compare: {"a": (lObj, rObj) => lObj + '' === rObj}})
+```
+<a name="Diff"></a>
+
+## Diff : <code>Object</code>
+**Kind**: global typedef  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| path | <code>String</code> | The dot notation path to the property with diff |
+| lObj | <code>Any</code> | The object or value of the left object compared |
+| rObj | <code>Any</code> | The object or value of the right object compared |
+
+<a name="GetDiffsOptions"></a>
+
+## GetDiffsOptions : <code>Object</code>
+**Kind**: global typedef  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| stopAt | <code>Object</code> \| <code>undefined</code> | Object with paths as keys (value='true') where those paths will not be descended; only to determine if there is a difference |
+| compare | <code>Object</code> \| <code>undefined</code> | Object with paths as keys and value a function which is to be used to determine if diff a that path; function takes 2 parameters: l
---
+++ test/get-diffs.test.js
@@ -0,0 +1,130 @@
+/** tests for get-diffs
+**/ 
+
+let should = require('should'); 
+let getDiffs = require('../lib').getDiffs;
+
+describe('getDiffs', () => {
+  it('should compare primitives', function() {
+    let d = getDiffs(4, 'a');
+    d.length.should.equal(1);
+    d[0].path.should.equal('');
+    d[0].lObj.should.equal(4);
+    d[0].rObj.should.equal('a');
+  }); // end of it
+  
+  it('should determine diffs for simple 1 level objects', function() {
+    let lo = {a: 1, b: 2, c: 5};
+    let ro = {a: 2, b: 2};
+    let d = getDiffs(lo, ro);
+    d.length.should.equal(2);
+    d[0].path.should.equal('a');
+    d[0].lObj.should.equal(1);
+    d[0].rObj.should.equal(2);
+    d[1].path.should.equal('c');
+    d[1].lObj.should.equal(5);
+    (d[1].rObj === undefined).should.equal(true);
+  }); // end of it
+
+  it('should ignore props in rObj that are not in lObj', function() {
+    let lo = {a: 1};
+    let ro = {a: 1, b: 2};
+    let d = getDiffs(lo, ro);
+    d.length.should.equal(0);
+  }); // end of it
+
+  it('should get diffs for multi level objects', function() {
+    let lo = {a: 1, b: {c: 2, d: 3, e: {f: 6}}};
+    let ro = {a: 1, b: {c: 2, d: 5, e: {g: 6}}};
+    let d = getDiffs(lo, ro);
+    d.length.should.equal(2);
+    d[0].path.should.equal('b.d');
+    d[0].lObj.should.equal(3);
+    d[0].rObj.should.equal(5);
+    d[1].path.should.equal('b.e.f');
+    d[1].lObj.should.equal(6);
+    (d[1].rObj === undefined).should.equal(true);
+  }); // end of it
+
+  it('should get diffs for nested arrays', function() {
+    let lo = {a: 1, b: [{c: 6}, {c: 8}, {c: 10}]};
+    let ro = {a: 1, b: [{c: 6}, {c: 9}]};
+    let d = getDiffs(lo, ro);
+    d.length.should.equal(2);
+    d[0].path.should.equal('b[1].c');
+    d[0].lObj.should.equal(8);
+    d[0].rObj.should.equal(9);
+    d[1].path.should.equal('b[2]');
+    d[1].lObj.c.should.equal(10);
+    (d[1].rObj === undefined).should.equal(true);
+  }); // end of it
+
+  it('should compare dates', function() {
+    let lo = {a: new Date('2017/01/01'), b: new Date('2018/01/01')};
+    let ro = {a: new Date('2017/12/01'), b: new Date('2018/01/01')};
+    let d = getDiffs(lo, ro);
+    d.length.should.equal(1);
+    d[0].path.should.equal('a');
+    d[0].lObj.should.equal(lo.a);
+    d[0].rObj.should.equal(ro.a);
+  }); // end of it
+
+  it('should compare different type', function() {
+    let lo = {a: new Date('2017/01/01'), b: new Date('2018/01/01')};
+    let ro = {a: 'q', b: new Date('2018/01/01')};
+    let d = getDiffs(lo, ro);
+    d.length.should.equal(1);
+    d[0].path.should.equal('a');
+    d[0].lObj.should.equal(lo.a);
+    d[0].rObj.should.equal(ro.a);
+  }); // end of it
+
+  it('should stop at a path if options indicate it', function() {
+    let lo = {a: {b: {c: 8}}};
+    let ro = {a: {b: {c: 7}}};
+    let d = getDiffs(lo, ro, undefined, {stopAt: {"a.b": true}});
+    d.length.should.equal(1);
+    d[0].path.should.equal('a.b');
+    d[0].lObj.should.match({c:8});
+    d[0].rObj.should.match({c:7});
+  }); // end of it
+
+  it('should stop at a path of array if options indicate it', function() {
+    let lo = {a: {b: [{c: 8}, {c:9}]}};
+    let ro = {a: {b: [{c: 8}, {c:10}]}};
+    let d = getDiffs(lo, ro, undefined, {stopAt: {"a.b": true}});
+    // console.log(d);
+    // console.log(d[0].lObj);
+    // console.log(d[0].rObj);
+    d.length.should.equal(1);
+    d[0].path.should.equal('a.b');
+    d[0].lObj.should.match([{c: 8}, {c:9}]);
+    d[0].rObj.should.match([{c: 8}, {c:10}]);
+  }); // end of it
+
+  it('should return no path if rObj is null', function() {
+    let lo = {a: 1, b: {c: 1}};
+    let ro ;
+    let d = getDiffs(lo, ro);
+    // console.log(d);
+    d.length.should.equal(1);
+    d[0].path.should.equal('');
+    d[0].lObj.should.match({a: 1, b: {c: 1}});
+    (ro === undefined).should.equal(true);
+  }); // end of it
+
+  it('should use an options compare', function() {
+    let lo = {a: 1, b: {z: 0, x: {y: 1}}};
+    let ro = {a: 1, b: {z: 0, x: {y: '1'}}};
+    let d = getDiffs(lo, ro);
+    d.length.should.equal(1);
+
+    let options = {compare: {'b.x.y': (lObj, rObj) => lObj + '' === rObj}};
+    d = getDiffs(lo, ro, undefined, options);
+    d.length.should.equal(0);
+  }); // end of it
+
+
+}); // end of diffs describe
+
+
